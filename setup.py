bf3a09545d729e8f70e5f6a9712ded75a57c846f
Olukunle Ogunmokun <kunsam002@gmail.com>
2019-01-11 20:57:37 +0100

    bug investigation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='vgg_sso',
-    version='1.0.10',
+    version='1.1.1',
     packages=['vgg_sso'],
     url='https://github.com/kunsam002/python_sso',
     license='',
