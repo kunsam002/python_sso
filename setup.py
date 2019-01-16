70210a1f17274b7c13520c287b9363198306b098
Olukunle Ogunmokun <kunsam002@gmail.com>
2019-01-16 10:13:30 +0100

    First Integration Test Required changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='vgg_sso',
     version='1.0.0',
     packages=['vgg_sso'],
-    url='https://github.com/kunsam002/python_sso',
+    url='https://github.com/kunsam002/vgg_sso',
     license='',
     author='kunsam002 - Olukunle Ogunmokun',
     author_email='kunsam002@gmail.com',
