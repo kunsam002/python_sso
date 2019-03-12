95385148023641910c916044f7449853acef1b10
Olukunle Ogunmokun <kunsam002@gmail.com>
2019-03-12 16:40:43 +0100

    SSO Clearance Maker-Checker Updates Bug Fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='vgg_sso',
-    version='3.0.0',
+    version='3.0.1',
     packages=['vgg_sso'],
     url='https://github.com/kunsam002/vgg_sso',
     license='',
